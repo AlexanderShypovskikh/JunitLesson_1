dc6856a74eddf4522372d1866a491973bb2418d1
Alexander Shypovskikh <core.editor>
2018-02-10 21:09:33 +0300

    first lesson of JUnit testing

---
+++ src/test/java/com/shypovskikh/project/MathTest.java
@@ -0,0 +1,18 @@
+package com.shypovskikh.project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathTest {
+
+    @Test
+    public void testThreeAndTwo(){
+        Math math = new Math(3,2 );
+        int res = math.getSum();
+
+        assertEquals(6, res);
+
+    }
+
+}
